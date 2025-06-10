bf5dfd0efd2862b6b9d9c6294e663205ee1ea229
apostoliseq <apostolisn@hotmail.com>
2025-06-10 21:09:18 +0300

    Add Total memory usage command

--- server-stats.sh
+++ server-stats.sh
@@ -6,7 +6,9 @@
 echo "CPU Usage: "$[100-$(vmstat 1 2|tail -1|awk '{print $15}')]"%"
 
 #     Total memory usage (Free vs Used including percentage)
-# free -h
+echo "Total Memory Usage: "$(free -h| awk '$1 == "Mem:" {print "total:"$2" " "used:"$3" " "free:"$4}')""
+echo "Used Memory:"$(free| awk '$1 == "Mem:" {print $3/$2 * 100 "%"}')""
+echo "Free Memory:"$(free| awk '$1 == "Mem:" {print $4/$2 * 100 "%"}')""
 #     Total disk usage (Free vs Used including percentage)
 #     Top 5 processes by CPU usage
 #     Top 5 processes by memory usage
